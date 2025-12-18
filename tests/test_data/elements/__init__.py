46c3a2ffc88a25e9505bbccccba099a19dbd2a54
Brendan Annable <brendan@annable.me>
2025-12-18 19:57:35 +1100

    Fix tests

--- tests/test_data/elements/__init__.py
+++ tests/test_data/elements/__init__.py
@@ -2,11 +2,12 @@
 
 from collections.abc import Mapping, Sequence
 
-from custom_components.haeo.elements import ELEMENT_TYPE_BATTERY, ELEMENT_TYPE_CONNECTION, ELEMENT_TYPE_GRID, ELEMENT_TYPE_LOAD, ELEMENT_TYPE_NODE, ElementConfigData, ElementConfigSchema, ElementType, ELEMENT_TYPE_PHOTOVOLTAICS
+from custom_components.haeo.elements import ELEMENT_TYPE_BATTERY, ELEMENT_TYPE_CONNECTION, ELEMENT_TYPE_GRID, ELEMENT_TYPE_INVERTER, ELEMENT_TYPE_LOAD, ELEMENT_TYPE_NODE, ElementConfigData, ElementConfigSchema, ElementType, ELEMENT_TYPE_PHOTOVOLTAICS
 
 from . import battery as battery_data
 from . import connection as connection_data
 from . import grid as grid_data
+from . import inverter as inverter_data
 from . import load as load_data
 from . import node as node_data
 from . import photovoltaics as pv_data
@@ -16,6 +17,7 @@ INVALID_SCHEMAS_BY_TYPE: Mapping[ElementType, Sequence[InvalidSchemaCase[Element
     ELEMENT_TYPE_BATTERY: battery_data.INVALID_SCHEMA,
     ELEMENT_TYPE_GRID: grid_data.INVALID_SCHEMA,
     ELEMENT_TYPE_CONNECTION: connection_data.INVALID_SCHEMA,
+    ELEMENT_TYPE_INVERTER: inverter_data.INVALID_SCHEMA,
     ELEMENT_TYPE_PHOTOVOLTAICS: pv_data.INVALID_SCHEMA,
     ELEMENT_TYPE_LOAD: load_data.INVALID_SCHEMA,
     ELEMENT_TYPE_NODE: node_data.INVALID_SCHEMA,
@@ -25,6 +27,7 @@ VALID_CONFIGS_BY_TYPE: Mapping[ElementType, Sequence[ElementValidCase[ElementCon
     ELEMENT_TYPE_BATTERY: battery_data.VALID,
     ELEMENT_TYPE_GRID: grid_data.VALID,
     ELEMENT_TYPE_CONNECTION: connection_data.VALID,
+    ELEMENT_TYPE_INVERTER: inverter_data.VALID,
     ELEMENT_TYPE_PHOTOVOLTAICS: pv_data.VALID,
     ELEMENT_TYPE_LOAD: load_data.VALID,
     ELEMENT_TYPE_NODE: node_data.VALID,
@@ -34,6 +37,7 @@ INVALID_MODEL_PARAMS_BY_TYPE: Mapping[ElementType, Sequence[InvalidModelCase[Ele
     ELEMENT_TYPE_BATTERY: battery_data.INVALID_MODEL_PARAMS,
     ELEMENT_TYPE_GRID: grid_data.INVALID_MODEL_PARAMS,
     ELEMENT_TYPE_CONNECTION: connection_data.INVALID_MODEL_PARAMS,
+    ELEMENT_TYPE_INVERTER: inverter_data.INVALID_MODEL_PARAMS,
     ELEMENT_TYPE_PHOTOVOLTAICS: pv_data.INVALID_MODEL_PARAMS,
     ELEMENT_TYPE_LOAD: load_data.INVALID_MODEL_PARAMS,
     ELEMENT_TYPE_NODE: node_data.INVALID_MODEL_PARAMS,
