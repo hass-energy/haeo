4d8ce0fd79e24112b16ea469e608c53c2116b989
Trent Houliston <trent@houliston.me>
2025-12-27 22:00:05 +1100

    refactor: move ELEMENT_TYPE constant to battery_balance_connection module
    Define ELEMENT_TYPE in battery_balance_connection.py and re-export as
    MODEL_ELEMENT_BATTERY_BALANCE_CONNECTION from model/__init__.py

--- custom_components/haeo/model/__init__.py
+++ custom_components/haeo/model/__init__.py
@@ -1,7 +1,6 @@
 """HAEO energy modeling components."""
 
-from typing import Final
-
+from .battery_balance_connection import ELEMENT_TYPE as MODEL_ELEMENT_BATTERY_BALANCE_CONNECTION
 from .battery_balance_connection import BatteryBalanceConnection as BatteryBalanceConnection
 from .connection import CONNECTION_OUTPUT_NAMES as CONNECTION_OUTPUT_NAMES
 from .connection import CONNECTION_POWER_SOURCE_TARGET as CONNECTION_POWER_SOURCE_TARGET
@@ -30,9 +29,6 @@ from .output_names import ModelOutputName
 from .power_connection import POWER_CONNECTION_OUTPUT_NAMES as POWER_CONNECTION_OUTPUT_NAMES
 from .power_connection import PowerConnection as PowerConnection
 from .power_connection import PowerConnectionOutputName as PowerConnectionOutputName
-
-# Model element type constant for balance connections (internal battery bookkeeping)
-MODEL_ELEMENT_BATTERY_BALANCE_CONNECTION: Final = "battery_balance_connection"
 
 __all__ = [
     "CONNECTION_OUTPUT_NAMES",
