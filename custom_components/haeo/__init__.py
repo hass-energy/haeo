2b76807635cedb9c21c78991d6249e407f14b385
Trent Houliston <trent@houliston.me>
2026-01-12 23:04:55 +1100

    Unload only input platforms

--- custom_components/haeo/__init__.py
+++ custom_components/haeo/__init__.py
@@ -184,7 +184,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: HaeoConfigEntry) -> bool
             await asyncio.gather(*[entity.wait_ready() for entity in runtime_data.input_entities.values()])
         _LOGGER.debug("All input entities ready")
     except TimeoutError:
-        await async_unload_entry(hass, entry)
+        # Unload input platforms to clean up partially created entities
+        await hass.config_entries.async_unload_platforms(entry, INPUT_PLATFORMS)
 
         not_ready = [key for key, entity in runtime_data.input_entities.items() if not entity.is_ready()]
         msg = f"Input entities not ready after 30s: {not_ready}"
