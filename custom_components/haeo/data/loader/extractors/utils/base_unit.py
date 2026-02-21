4bea10e7b7c682c5bfc6e23eaa8e85e5ff9de063
Brendan Annable <brendan@annable.id.au>
2026-02-21 14:46:38 +1100

    .

--- custom_components/haeo/data/loader/extractors/utils/base_unit.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility re-exports for core unit conversion helpers."""
-
-from custom_components.haeo.core.units import base_unit_for_device_class, convert_to_base_unit
-
-__all__ = ["base_unit_for_device_class", "convert_to_base_unit"]
