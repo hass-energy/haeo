a375e45d665cdf158795ee44459953efe2c42fba
Brendan Annable <brendan@annable.id.au>
2026-02-23 14:15:27 +1100

    .

---
+++ custom_components/haeo/core/schema/sections/__init__.py
@@ -0,0 +1,59 @@
+"""Section type definitions for HAEO element configuration."""
+
+from .common import (
+    CONF_CONNECTION,
+    SECTION_COMMON,
+    CommonConfig,
+    CommonData,
+    ConnectedCommonConfig,
+    ConnectedCommonData,
+)
+from .curtailment import CONF_CURTAILMENT, SECTION_CURTAILMENT, CurtailmentConfig, CurtailmentData
+from .efficiency import (
+    CONF_EFFICIENCY_SOURCE_TARGET,
+    CONF_EFFICIENCY_TARGET_SOURCE,
+    SECTION_EFFICIENCY,
+    EfficiencyConfig,
+    EfficiencyData,
+)
+from .forecast import CONF_FORECAST, SECTION_FORECAST, ForecastConfig, ForecastData
+from .power_limits import (
+    CONF_MAX_POWER_SOURCE_TARGET,
+    CONF_MAX_POWER_TARGET_SOURCE,
+    SECTION_POWER_LIMITS,
+    PowerLimitsConfig,
+    PowerLimitsData,
+)
+from .pricing import CONF_PRICE_SOURCE_TARGET, CONF_PRICE_TARGET_SOURCE, SECTION_PRICING, PricingConfig, PricingData
+
+__all__ = [
+    "CONF_CONNECTION",
+    "CONF_CURTAILMENT",
+    "CONF_EFFICIENCY_SOURCE_TARGET",
+    "CONF_EFFICIENCY_TARGET_SOURCE",
+    "CONF_FORECAST",
+    "CONF_MAX_POWER_SOURCE_TARGET",
+    "CONF_MAX_POWER_TARGET_SOURCE",
+    "CONF_PRICE_SOURCE_TARGET",
+    "CONF_PRICE_TARGET_SOURCE",
+    "SECTION_COMMON",
+    "SECTION_CURTAILMENT",
+    "SECTION_EFFICIENCY",
+    "SECTION_FORECAST",
+    "SECTION_POWER_LIMITS",
+    "SECTION_PRICING",
+    "CommonConfig",
+    "CommonData",
+    "ConnectedCommonConfig",
+    "ConnectedCommonData",
+    "CurtailmentConfig",
+    "CurtailmentData",
+    "EfficiencyConfig",
+    "EfficiencyData",
+    "ForecastConfig",
+    "ForecastData",
+    "PowerLimitsConfig",
+    "PowerLimitsData",
+    "PricingConfig",
+    "PricingData",
+]
---
+++ custom_components/haeo/core/schema/sections/common.py
@@ -0,0 +1,36 @@
+"""Section types for common configuration."""
+
+from typing import Final, NotRequired, TypedDict
+
+from custom_components.haeo.schema import ConnectionTarget
+
+SECTION_COMMON: Final = "common"
+CONF_CONNECTION: Final = "connection"
+
+
+class CommonConfig(TypedDict):
+    """Common configuration for element identity and connectivity."""
+
+    name: str
+    connection: NotRequired[ConnectionTarget]
+
+
+class CommonData(TypedDict):
+    """Loaded common values for element identity and connectivity."""
+
+    name: str
+    connection: NotRequired[ConnectionTarget]
+
+
+class ConnectedCommonConfig(TypedDict):
+    """Common configuration with a required connection target."""
+
+    name: str
+    connection: ConnectionTarget
+
+
+class ConnectedCommonData(TypedDict):
+    """Loaded common values with a required connection target."""
+
+    name: str
+    connection: ConnectionTarget
