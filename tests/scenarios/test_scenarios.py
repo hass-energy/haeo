625c3c5452b9700ad65b344d368eec3ec83d4625
Trent Houliston <trent@houliston.me>
2026-03-05 09:06:27 +1100

    Separate scenario and guide tests into own CI jobs

--- tests/scenarios/test_scenarios.py
+++ tests/scenarios/test_scenarios.py
@@ -2,7 +2,6 @@
 
 import asyncio
 import logging
-import os
 from pathlib import Path
 from types import MappingProxyType
 from typing import Any
@@ -52,8 +51,6 @@ def _discover_scenarios() -> list[Path]:
 _scenarios = _discover_scenarios()
 
 
-# Skip if in CI
-@pytest.mark.skipif(os.getenv("CI") == "true", reason="Skipping scenario tests in CI")
 @pytest.mark.scenario
 @pytest.mark.timeout(30)
 @pytest.mark.parametrize(
